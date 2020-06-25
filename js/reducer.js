894c50bd8768fc21d7b5b5c716e9de845960c746
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-06-25 02:35:51 +0000

    Automatically backed up by Learn

--- js/reducer.js
+++ js/reducer.js
@@ -1,7 +1,7 @@
 function changeState(state, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
-      return {count: state.count + 1}
+      return {count: state.count + 1};
     default:
       return state;
   }
